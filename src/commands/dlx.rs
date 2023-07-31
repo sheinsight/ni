9a7f2c2122d396c67b1340565bd97e9d75992f82
ityuany <519495771@qq.com>
2023-07-31 21:49:36 +0800

    fix:  n x  Failed to pass the parameters correctly.

--- src/commands/dlx.rs
+++ src/commands/dlx.rs
@@ -6,12 +6,20 @@ pub struct DlxArgs {
     /// Target package
     #[arg(value_name = "package")]
     pub package: String,
+
+    #[arg(value_name = "--")]
+    pub pass_on: Vec<String>,
 }
 
 pub fn handler(package_manager: &String, dlx_args: DlxArgs) {
-    let DlxArgs { package } = dlx_args;
+    let DlxArgs { package, pass_on } = dlx_args;
     match package_manager.as_str() {
-        "npm" => run_shell(format!("npx {}", package)),
-        _ => run_shell(format!("{} dlx {}", package_manager, package)),
+        "npm" => run_shell(format!("npx {} {}", package, pass_on.join(" "))),
+        _ => run_shell(format!(
+            "{} dlx {} {}",
+            package_manager,
+            package,
+            pass_on.join(" ")
+        )),
     }
 }
